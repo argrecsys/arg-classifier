5abf9600c3853868140bf0562a8c32da87befde4
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-05-11 22:55:52 +0200

    Update DataProcessor
    The first version of data processor module has been uploaded.

--- code/DataProcessor/src/main.py
+++ code/DataProcessor/src/main.py
@@ -8,7 +8,7 @@
 """
 
 # Import Custom libraries
-import util_lib as cul
+from util import files as fl
 
 # Import Python base libraries
 from datetime import datetime
@@ -20,26 +20,26 @@ from datetime import datetime
 # Read data configuration
 def read_app_setup() -> dict:
     filepath = "../config/config.json"
-    setup = cul.get_dict_from_json(filepath)
+    setup = fl.get_dict_from_json(filepath)
     return setup
 
 # Read JSON input dataset
 def read_input_dataset(folder_path:str) -> dict:
     filepath = folder_path + "annotations.jsonl"
-    dataset = cul.get_list_from_jsonl(filepath)
+    dataset = fl.get_list_from_jsonl(filepath)
     return dataset
 
 # Processing dataset from JSON to CSV
-def process_dataset(in_dataset:list) -> list:
+def process_dataset(in_dataset:list, language:str) -> list:
     out_dataset = []
     
     return out_dataset
 
 # Save CSV output dataset
 def save_output_dataset(dataset:list, folder_path:str) -> bool:
-    result = False
-    filepath = folder_path + "input_data.jsonl"
-    
+    filepath = folder_path + "sentences.csv"
+    header = [""]
+    result = fl.save_csv_data(filepath, header, dataset)
     return result
 
 # Start application
@@ -57,7 +57,7 @@ def start_app():
         print(" - Total number of records read:", len(json_dataset))
         
         # 2. Processing dataset
-        csv_dataset = process_dataset(json_dataset)
+        csv_dataset = process_dataset(json_dataset, language)
         
         # 3. Save CSV output dataset
         result = save_output_dataset(csv_dataset, data_folder)
--- code/DataProcessor/src/util/files.py
+++ code/DataProcessor/src/util/files.py
@@ -3,8 +3,8 @@
     Created by: Andres Segura Tinoco
     Version: 1.0.0
     Created on: Oct 06, 2021
-    Updated on: Oct 19, 2021
-    Description: Library with utility functions
+    Updated on: May 11, 2022
+    Description: Files library with utility functions
 """
 
 # Import Python base libraries
@@ -13,9 +13,6 @@ import csv
 import json
 import yaml
 import pandas as pd
-
-# Import ML libraries
-from sklearn.feature_extraction.text import CountVectorizer
 
 # File function - Read list from plain file
 def get_list_from_plain_file(filepath:str, encoding:str="utf-8") -> list:
@@ -80,9 +77,8 @@ def get_max_value_from_csv_file(filepath:str, column:str) -> int:
     return max_value
 
 # File function - Save or update CSV data
-def save_append_csv_data(filepath:str, header:list, data:list, encoding:str="utf-8") -> bool:
+def save_csv_data(filepath:str, header:list, data:list, mode:str="w", encoding:str="utf-8") -> bool:
     result = False
-    mode = "a" if os.path.exists(filepath) else "w"
     
     try:    
         with open(filepath, mode, newline="", encoding=encoding) as f:
@@ -97,42 +93,3 @@ def save_append_csv_data(filepath:str, header:list, data:list, encoding:str="utf
         print("Error:", e)
     
     return result
-
-# Util function - Convert the values of a dict of dicts to a list
-def convert_dict_dict_to_list(dict_dict:dict, key:str) -> list:
-    values = []
-    
-    for k, v in dict_dict.items():
-        value = v.get(key, "")
-        values.append(value)
-    
-    return values
-        
-# Util function - Transform a categorical list to a numeric list
-def convert_categ_to_num(catg_list:list) -> dict:
-    label_dict = {}
-    
-    for value in catg_list:
-        value = value.lower()
-        if not value in label_dict:
-            label_dict[value] = len(label_dict)
-    
-    return label_dict
-
-# Util function - Calculate DataFrame sparsity
-def calc_df_sparsity(df:pd.DataFrame) -> float:
-    sparsity = (df.to_numpy() == 0).mean()
-    return sparsity
-
-# Util function - Creates a DataFrame from a sparse matrix using CountVectorizer data structure
-def create_df_from_sparse_matrix(matrix:list) -> pd.DataFrame:
-    
-    # Word vectorization
-    vectorizer = CountVectorizer(analyzer=lambda x: x)
-    data = vectorizer.fit_transform(matrix).toarray()
-    columns = vectorizer.get_feature_names()
-        
-    # Create dataframe
-    df = pd.DataFrame(data, columns=columns)
-    
-    return df
